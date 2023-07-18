56decac92198e644cd283a8d29c4840b199f8c8a
Karen Shirvanyan <shivi@karens-mbp.lan>
2023-07-17 18:23:06 -0700

    added all variables for citystay

--- assets/java/cityStay.js
+++ assets/java/cityStay.js
@@ -1,1 +1,12 @@
 console.log(localStorage);
+var storedData = localStorage.getItem('data');
+var dataArray = JSON.parse(storedData);
+var storedCity = localStorage.getItem('city');
+var geoId = localStorage.getItem('gedId');
+var searchButton = $('#searchBtn');
+var numRoom = $('roomNum')
+var numAdults = $('adultsNum');
+var numChild = $('childNum');
+var priceMin = $('minPrice');
+var priceMax = $('maxPrice');
+var rating = $('rating')
